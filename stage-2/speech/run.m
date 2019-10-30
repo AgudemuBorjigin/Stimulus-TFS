5e5ec8d270955246e4d220ff2c35b8bf4154445e
snaplab <snaplab>
2019-10-30 16:39:19 -0400

    ready for testing

--- stage-2/speech/run.m
+++ stage-2/speech/run.m
@@ -1,9 +1,22 @@
 subj = input('Please subject ID:', 's');
+
+flag_cond = 1;
+while flag_cond
+    condition = input('Test condition (anechoic, echo, space, pitch, or sum): ', 's');
+    switch condition
+        case {'anechoic', 'echo', 'space', 'pitch', 'sum'}
+            flag_cond = 0;
+        otherwise
+            fprintf(2, 'Unrecognized stimulus type! Try again!\n');
+    end
+end
+
 % Make directory to save results
-paraDir = 'C:\Experiments\Agudemu\stimulus-TFS\stage-2\results\';
+paraDir = 'C:\Experiments\Agudemu\stimulus-TFS\stage-2\speech\results\';
 if(~exist(strcat(paraDir,'\',subj),'dir'))
     mkdir(strcat(paraDir,'\',subj));
 end
+
 startflag = input('Start from the beginning? Y/N:', 's');
 if (startflag == 'Y' || startflag == 'y')
     startBlock = 1;
@@ -11,7 +24,7 @@ else
     startBlock = input('Enter the block number at which to start (1-10):');
 end
 respDir = strcat(paraDir,'\',subj,'\');
-save('currsubj', 'subj', 'respDir', 'startBlock');
+save('currsubj', 'subj', 'respDir', 'startBlock', 'condition');
 
 %% Call app
 pause(1);
