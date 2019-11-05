b67d7aeb98628c16f7fbe359b75f0b57e53b2b14
agudemuborjigan <agudemubor@gmail.com>
2019-11-04 22:03:06 -0500

    ready for testing

--- stage-2/speech/audio/conditions.m
+++ stage-2/speech/audio/conditions.m
@@ -40,59 +40,64 @@ b_opposite = filter_param(num_trials, gender, strcat(root_audios, '/target_maske
 
 if strcmp(configuration,'anechoic')
     num_trial = 0;
+    rand_trialnums = randperm(sum(N));
     for i = 1:num_snr
         for j = 1:N(i)
             num_trial = num_trial + 1;
             load(strcat(root_audios, '/target_masker/same_gender/trial', int2str(num_trial), '.mat'));
-            mixture(stim_tar, stim_same, b_same, configuration, SNRs(i),  int2str(num_trial), target, wordlist, t_onset, fs, rampdur, root_audios);
+            mixture(stim_tar, stim_same, b_same, configuration, SNRs(i),  int2str(rand_trialnums(num_trial)), target, wordlist, t_onset, fs, rampdur, root_audios);
         end
     end
 elseif strcmp(configuration, 'pitch')
     num_trial = 0;
+    rand_trialnums = randperm(sum(N));
     for i = 1:num_snr
         for j = 1:N(i)
             num_trial = num_trial + 1;
             load(strcat(root_audios, '/target_masker/opposite_gender/trial', int2str(num_trial), '.mat'));
-            mixture(stim_tar, stim_opposite, b_opposite, configuration, SNRs(i), int2str(num_trial), target, wordlist, t_onset, fs, rampdur, root_audios);
+            mixture(stim_tar, stim_opposite, b_opposite, configuration, SNRs(i), int2str(rand_trialnums(num_trial)), target, wordlist, t_onset, fs, rampdur, root_audios);
         end
     end
 elseif strcmp(configuration, 'space')
     num_trial = 0;
+    rand_trialnums = randperm(sum(N));
     for i = 1:num_snr
         for j = 1:N(i)
             num_trial = num_trial + 1;
             load(strcat(root_audios, '/target_masker/same_gender/trial', int2str(num_trial), '.mat'));
-            mixture(stim_tar, stim_same, b_same, configuration, SNRs(i),  int2str(num_trial), target, wordlist, t_onset, fs, rampdur, root_audios);
+            mixture(stim_tar, stim_same, b_same, configuration, SNRs(i),  int2str(rand_trialnums(num_trial)), target, wordlist, t_onset, fs, rampdur, root_audios);
         end
     end
 elseif strcmp(configuration, 'echo')
     num_trial = 0;
+    rand_trialnums = randperm(sum(N));
     for i = 1:num_snr
         for j = 1:N(i)
             num_trial = num_trial + 1;
             load(strcat(root_audios, '/target_masker/same_gender/trial', int2str(num_trial), '.mat'));
-            mixture(stim_tar, stim_same, b_same, configuration, SNRs(i),  int2str(num_trial), target, wordlist, t_onset, fs, rampdur, root_audios);
+            mixture(stim_tar, stim_same, b_same, configuration, SNRs(i),  int2str(rand_trialnums(num_trial)), target, wordlist, t_onset, fs, rampdur, root_audios);
         end
     end
 elseif strcmp(configuration, 'sum')
     num_trial = 0;
+    rand_trialnums = randperm(sum(N));
     for i = 1:num_snr
         for j = 1:N(i)
             num_trial = num_trial + 1;
             load(strcat(root_audios, '/target_masker/opposite_gender/trial', int2str(num_trial), '.mat'));
-            mixture(stim_tar, stim_opposite, b_opposite, configuration, SNRs(i), int2str(num_trial), target, wordlist, t_onset, fs, rampdur, root_audios);
+            mixture(stim_tar, stim_opposite, b_opposite, configuration, SNRs(i), int2str(rand_trialnums(num_trial)), target, wordlist, t_onset, fs, rampdur, root_audios);
         end
     end
 end
 
-% ordered snrs for each repetition
-SNRcounts = zeros(nconds, 1);
-SNRlist = [];
-for rep = 1:max(N)
-    for k = 1:nconds
-        if SNRcounts(k) <= N(k)
-            SNRlist = [SNRlist, SNRs(k)]; %#ok<AGROW>
-            SNRcounts(k) = SNRcounts(k) + 1;
-        end
-    end
-end
+% % ordered snrs for each repetition
+% SNRcounts = zeros(nconds, 1);
+% SNRlist = [];
+% for rep = 1:max(N)
+%     for k = 1:nconds
+%         if SNRcounts(k) <= N(k)
+%             SNRlist = [SNRlist, SNRs(k)]; %#ok<AGROW>
+%             SNRcounts(k) = SNRcounts(k) + 1;
+%         end
+%     end
+% end
