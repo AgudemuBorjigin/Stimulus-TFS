16e9fb88dad44b0ad4d0e04c0b3a9926049877bb
snaplab <snaplab>
2019-11-14 15:37:25 -0500

    pilot

--- stage-2/speech/plotResults.m
+++ stage-2/speech/plotResults.m
@@ -1,5 +1,5 @@
-subj = 'S133';
-cond = 'pitch';
+subj = 'Britany';
+cond = 'space';
 flag_c = 1;
 while flag_c
     switch cond
@@ -7,13 +7,13 @@ while flag_c
             SNRs = -22:5:8;
             flag_c = 0;
         case 'pitch'
-            SNRs = -15:5:10;
+            SNRs = -16:4:12;
             flag_c = 0;
         case 'space'
-            SNRs = -15:5:10; 
+            SNRs = -16:4:12; 
             flag_c = 0;
         case 'echo'
-            SNRs = -10:4:10; 
+            SNRs = -16:4:12; 
             flag_c = 0;
         case 'sum'
             SNRs = -18:4:2;
@@ -24,17 +24,17 @@ while flag_c
 end
 rootPath = 'C:\Experiments\Agudemu\stimulus-TFS\stage-2\speech\results\';
 files = dir(strcat(rootPath, cond, '\', subj));
-load(strcat(rootPath, cond, '\', subj, '\', files(end-1).name));
+load(strcat(rootPath, cond, '\', subj, '\', files(end).name));
 
 for k = 1:numel(SNRs)
     resp_SNR = responseTable(responseTable(:, 4) == SNRs(k), 2);
     target_SNR = responseTable(responseTable(:, 4) == SNRs(k), 3);
     ntrials(k) = sum(responseTable(:, 4) == SNRs(k)); %#ok<SAGROW>
-    score(k) = sum(resp_SNR == target_SNR) / ntrials(k); %#ok<SAGROW>
-    scorestd(k) = score(k)*(1-score(k))/sqrt(ntrials(k)); %#ok<SAGROW>
+    scores(k) = sum(resp_SNR == target_SNR) / ntrials(k); %#ok<SAGROW>
+    scorestd(k) = scores(k)*(1-scores(k))/sqrt(ntrials(k)); %#ok<SAGROW>
     snr_label{k} = num2str(SNRs(k)); %#ok<SAGROW>
 end
-errorbar(SNRs, score, scorestd, 'xb', 'linew', 2);
+errorbar(SNRs, scores, scorestd, 'xb', 'linew', 2);
 title(strcat(cond, '-', subj));
 xlabel('SNR (dB)', 'fontsize', 16);
 ylabel('Proportion Correct', 'fontsize', 16);
